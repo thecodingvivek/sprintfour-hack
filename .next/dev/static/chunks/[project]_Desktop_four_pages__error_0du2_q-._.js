ffc9bab1e8f2a787428d623b14048dd53112e491
vivek chitturi <139134664+thecodingvivek@users.noreply.github.com>
2026-06-30 20:31:04 +0530

    remove next folder

--- .next/dev/static/chunks/[project]_Desktop_four_pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/1bws_next_dist_compiled_0tczfjr._.js","static/chunks/1bws_next_dist_shared_lib_1-804gh._.js","static/chunks/1bws_next_dist_client_1l6o769._.js","static/chunks/1bws_next_dist_07-ka-j._.js","static/chunks/0ka2_next_dist_pages__error_0qg--9j.js","static/chunks/[next]_entry_page-loader_ts_1j_ovv3._.js","static/chunks/1bws_react-dom_1h7d1oq._.js","static/chunks/1bws_05sepj1._.js","static/chunks/[root-of-the-server]__1qd08y0._.js"],
-    source: "entry"
-});
